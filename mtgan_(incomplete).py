d3535ca4b1f7fcc999929f05bfe0b7b1415c879b
simonou99 <ouzhongran@gmail.com>
2023-03-28 18:27:08 +1300

    update class version of the gan

--- mtgan_(incomplete).py
+++ mtgan_(incomplete).py
@@ -8,11 +8,11 @@ class TrafficGENm:
         self.generator_collection{generator.data_name} = generator.generator
         
 class Generator:
-    def __init__(self, is_temporal=False, use_conv=False, use_wasserstein=True, n_layers=3, nodes_per_layer=64):
+    def __init__(self, is_sequential=False, use_conv=False, use_wasserstein=True, n_layers=3, nodes_per_layer=64):
         self.generator = None
         self.data_name = None
         self.parameters = {}
     
 class Discriminator:
-    def __init__(self, is_temporal=False, use_conv=False, use_wasserstein=True, n_layers=3, nodes_per_layer=64):
+    def __init__(self, is_sequential=False, use_conv=False, use_wasserstein=True, n_layers=3, nodes_per_layer=64):
         self.discriminator = None
